6442b81989278eb161e144ccdb846d93704a1a6d
Karim Kameka <kamekk@hotmail.com>
2020-04-27 14:23:23 -0400

    Added function to find nearest by Zip code

--- HealthBotLocations/Functions/LocationLookup.cs
+++ HealthBotLocations/Functions/LocationLookup.cs
@@ -7,7 +7,6 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 /// <summary>
--- HealthBotLocations/Models/WayPoint.cs
+++ HealthBotLocations/Models/WayPoint.cs
@@ -6,5 +6,22 @@
         public double Longitude { get; set; }
 
         public string Coordinates => $"{Latitude},{Longitude}";
+
+        public WayPoint()
+        {
+
+        }
+
+        public WayPoint(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public WayPoint(Point point)
+        {
+            Latitude = point.Coordinates[0];
+            Longitude = point.Coordinates[1];
+        }
     }
 }
